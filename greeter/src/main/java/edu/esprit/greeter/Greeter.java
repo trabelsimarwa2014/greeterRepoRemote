45ef11d9b348ba80580a92730268380dca5f29d8
marwa <marwa_trabelsi@hotmail.fr>
2015-10-29 11:49:15 +0100

    Signed-off-by: marwa <marwa_trabelsi@hotmail.fr>

--- greeter/src/main/java/edu/esprit/greeter/Greeter.java
+++ greeter/src/main/java/edu/esprit/greeter/Greeter.java
@@ -1,6 +1,17 @@
 package edu.esprit.greeter;
 
 public class Greeter {
+
+	public String var1;
+	
+	
+	public String var2;
+
+	/**
+	 * sayHello method
+	 * @param name
+	 * @return "Hello" + name
+	 */
 public String sayHello(String name)
 {
 	if(name.equals("MAN"))
